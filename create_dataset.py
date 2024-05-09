11b2f8881ce19560f1cd55451dfe48e994c37094
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-05-09 22:21:55 +0200

    Extract more temporal ner types

--- create_dataset.py
+++ create_dataset.py
@@ -13,7 +13,7 @@ def contains_temporal_expression(text, client):
     doc = client.annotate(text)
     for sentence in doc.sentence:
         for token in sentence.token:
-            if token.ner == "DATE":
+            if token.ner in ["DATE", "TIME", "DURATION", "SET"]:
                 if not token.timexValue.text in [e.text for e in temporal_expressions]:
                     temporal_expression_bool = True
                     temporal_expressions.append(token.timexValue)
