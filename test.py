9ef8380d6c36f6bfbc0ec0684fc4073b2a7ecf26
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-05-28 16:43:19 +0200

    Add is_offset.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from utils.periods.compute_similarity_periods import compute_similarity_periods
 from utils.mappings.offset_to_text import offset_to_text
 from utils.mappings.date_to_text import date_to_text
 from utils.mappings.period_to_text import period_to_text
+from utils.offsets.is_offset import is_offset
 
 data_folder_path = "data"
 input_file_name = "one_sentence/one_sentence.json"
@@ -16,12 +17,8 @@ with open(os.path.join(data_folder_path, input_file_name), "r", encoding="utf-8"
     input_data = json.load(f)
 
     for element in tqdm(input_data):
-        text = offset_to_text(element["value"])
-        if text:
-            print(f"{element['value']}: {text}")
-        text = date_to_text(element["value"])
-        if text:
-            print(f"{element['value']}: {text}")
-        text = period_to_text(element["value"])
-        if text:
-            print(f"{element['value']}: {text}")
+        is_format_bool, text = is_offset(element["value"])
+        if is_format_bool:
+            print(f"=================== {element['value']} :: {text}")
+        else:
+            print(f"{element['value']}")
