0bca4effbea6191436f8f99504473c0bea34da61
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-05-29 16:55:04 +0200

    First iteration

--- generate_base_dataset.py
+++ generate_base_dataset.py
@@ -4,24 +4,35 @@ from utils.mappings.expression_to_text import expression_to_text
 from utils.dates.generate_random import generate_random_date_full
 import os
 import json
+from tqdm import tqdm
 
 DATA_FOLDER_PATH = "data/base_dataset"
 OUTPUT_FILE_NAME = "base_dataset.json"
 
 output_data = []
 
-for i in  range(1000):
+for i in  tqdm(range(1000)):
     first_random_temporal_expression = generate_random_temporal_expression()
-    current_date = generate_random_date_full(1900, 2050)
+    current_date = generate_random_date_full(2000, 2024)
+    year = int(current_date.split("-")[0])
+    if 2000 < year < 2024:
+        start_year = year - 3
+        end_year = year + 3
+    else:
+        start_year = 2000
+        end_year = 2024
+    current_date_target = generate_random_date_full(start_year, end_year)
     sentence = f"[CLS] {expression_to_text(first_random_temporal_expression)} [SEP] {expression_to_text(current_date)} [SEP]"
     for j in range(5):
         second_random_temporal_expression = generate_random_temporal_expression()
-        similarity = compute_similarity_expressions(first_random_temporal_expression, second_random_temporal_expression, current_date)
-        output_data.append((sentence, expression_to_text(second_random_temporal_expression), similarity))
+        similarity = compute_similarity_expressions(first_random_temporal_expression, second_random_temporal_expression, current_date, current_date_target)
+        sentence_target = f"[CLS] {expression_to_text(second_random_temporal_expression)} [SEP] {expression_to_text(current_date_target)} [SEP]"
+        output_data.append((sentence, sentence_target, similarity))
     for j in range(5):
         second_random_temporal_expression = generate_close_random_temporal_expression(first_random_temporal_expression)
-        similarity = compute_similarity_expressions(first_random_temporal_expression, second_random_temporal_expression, current_date)
-        output_data.append((sentence, expression_to_text(second_random_temporal_expression), similarity))
+        similarity = compute_similarity_expressions(first_random_temporal_expression, second_random_temporal_expression, current_date, current_date_target)
+        sentence_target = f"[CLS] {expression_to_text(second_random_temporal_expression)} [SEP] {expression_to_text(current_date_target)} [SEP]"
+        output_data.append((sentence, sentence_target, similarity))
 
 with open(os.path.join(DATA_FOLDER_PATH, OUTPUT_FILE_NAME), "w", encoding="utf-8") as f:
     json.dump(output_data, f, indent=4)
--- utils/compute_similarity_expressions.py
+++ utils/compute_similarity_expressions.py
@@ -5,7 +5,7 @@ from utils.dates.is_date import is_date
 from utils.periods.is_period import is_period
 from utils.offsets.is_offset import is_offset
 
-def compute_similarity_expressions(first_expression, second_expression, current_date):
+def compute_similarity_expressions(first_expression, second_expression, first_current_date, second_current_date):
     if is_date(first_expression)[0]:
         if is_date(second_expression)[0]:
             return compute_similarity_dates(first_expression, second_expression)
@@ -18,6 +18,6 @@ def compute_similarity_expressions(first_expression, second_expression, current_
             return 0.0
     elif is_offset(first_expression)[0]:
         if is_offset(second_expression)[0]:
-            return compute_similarity_offsets(first_expression, is_offset(first_expression)[1], second_expression, is_offset(second_expression)[1], current_date)
+            return compute_similarity_offsets(first_expression, is_offset(first_expression)[1], second_expression, is_offset(second_expression)[1], first_current_date, second_current_date)
         else:
             return 0.0
