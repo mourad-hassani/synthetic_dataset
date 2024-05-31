26d78b1fc2fe029c7068efd50dfea0e9496e8215
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-05-31 11:56:13 +0200

    Add refs

---
+++ utils/refs/to_date.py
@@ -0,0 +1,9 @@
+def to_date(annotation, current_date):
+    if annotation == "PRESENT_REF":
+        return current_date
+    if annotation == "THIS NI":
+        return f"{current_date}TNI"
+    if annotation == "THIS MO":
+        return f"{current_date}TMO"
+    
+    return None
