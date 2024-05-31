a5efbb7d5b7fab2080ccf31b26a81020535e0325
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-05-31 18:04:03 +0200

    Add more types to to_date function

--- utils/offsets/generate_random_offset.py
+++ utils/offsets/generate_random_offset.py
@@ -4,10 +4,21 @@ from utils.extract_integers import extract_integers
 def generate_random_offset():
     rand_int = random.randint(1, 30)
     is_negative = bool(random.getrandbits(1))
-    rand_format = random.randint(0, 7)
+    rand_format = random.randint(0, 19)
+    rand_bool = bool(random.getrandbits(1))
 
     if is_negative:
         rand_int *= -1
+    
+    if rand_bool:
+        time_text = "MO"
+    else:
+        time_text = "NI"
+    
+    if rand_bool:
+        immediate_text = "NEXT_IMMEDIATE"
+    else:
+        immediate_text = "PREV_IMMEDIATE"
     
     if rand_format == 0:
         return f"OFFSET P{rand_int}D"
@@ -25,12 +36,36 @@ def generate_random_offset():
         return f"THIS P{abs(rand_int)}M OFFSET P{rand_int}M"
     elif rand_format == 7:
         return f"THIS P{abs(rand_int)}Y OFFSET P{rand_int}Y"
+    elif rand_format == 8:
+        return f"T{time_text} OFFSET P{rand_int}D"
+    elif rand_format == 9:
+        return f"T{time_text} OFFSET P{rand_int}W"
+    elif rand_format == 10:
+        return f"T{time_text} OFFSET P{rand_int}M"
+    elif rand_format == 11:
+        return f"T{time_text} OFFSET P{rand_int}Y"
+    elif rand_format == 12:
+        return f"THIS P{rand_int}D"
+    elif rand_format == 13:
+        return f"THIS P{rand_int}W"
+    elif rand_format == 14:
+        return f"THIS P{rand_int}M"
+    elif rand_format == 15:
+        return f"THIS P{rand_int}Y"
+    elif rand_format == 16:
+        return f"{immediate_text} P{rand_int}D"
+    elif rand_format == 17:
+        return f"{immediate_text} P{rand_int}W"
+    elif rand_format == 18:
+        return f"{immediate_text} P{rand_int}M"
+    elif rand_format == 19:
+        return f"{immediate_text} P{rand_int}Y"
 
 def generate_close_random_offset(value, type):
-    if type in ["d", "w", "m", "y"]:
-        value = extract_integers(value)[0]
-    else:
+    if type in ["td", "tw", "tm", "ty"]:
         value = extract_integers(value)[1]
+    else:
+        value = extract_integers(value)[0]
         
     is_negative = True if value < 0 else False
     value = abs(value)
@@ -40,6 +75,18 @@ def generate_close_random_offset(value, type):
 
     if is_negative:
         rand_int *= -1
+
+    rand_bool = bool(random.getrandbits(1))
+
+    if rand_bool:
+        time_text = "MO"
+    else:
+        time_text = "NI"
+    
+    if rand_bool:
+        immediate_text = "NEXT_IMMEDIATE"
+    else:
+        immediate_text = "PREV_IMMEDIATE"
     
     if type == "d":
         return f"OFFSET P{rand_int}D"
@@ -57,3 +104,27 @@ def generate_close_random_offset(value, type):
         return f"THIS P{abs(rand_int)}M OFFSET P{rand_int}M"
     elif type == "ty":
         return f"THIS P{abs(rand_int)}Y OFFSET P{rand_int}Y"
+    elif type == "timed":
+        return f"T{time_text} OFFSET P{rand_int}D"
+    elif type == "timew":
+        return f"T{time_text} OFFSET P{rand_int}W"
+    elif type == "timem":
+        return f"T{time_text} OFFSET P{rand_int}M"
+    elif type == "timey":
+        return f"T{time_text} OFFSET P{rand_int}Y"
+    elif type == "thisd":
+        return f"THIS P{rand_int}D"
+    elif type == "thisw":
+        return f"THIS P{rand_int}W"
+    elif type == "thism":
+        return f"THIS P{rand_int}M"
+    elif type == "thisy":
+        return f"THIS P{rand_int}Y"
+    elif type == "immediated":
+        return f"{immediate_text} P{rand_int}D"
+    elif type == "immediatew":
+        return f"{immediate_text} P{rand_int}W"
+    elif type == "immediatem":
+        return f"{immediate_text} P{rand_int}M"
+    elif type == "immediatey":
+        return f"{immediate_text} P{rand_int}Y"
--- utils/refs/to_date.py
+++ utils/refs/to_date.py
@@ -1,9 +1,9 @@
 def to_date(annotation, current_date):
     if annotation == "PRESENT_REF":
-        return current_date
+        return [current_date]
     if annotation == "THIS NI":
-        return f"{current_date}TNI"
+        return [f"{current_date}TNI"]
     if annotation == "THIS MO":
-        return f"{current_date}TMO"
+        return [f"{current_date}TMO"]
     
     return None
