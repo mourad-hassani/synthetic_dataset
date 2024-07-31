0f70fe101afd1113f8c5c5704ddcdca81401d2e7
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-07-31 15:58:58 +0200

    Remove devision by zero in compute_similarity_periods.py file

--- utils/periods/compute_similarity_periods.py
+++ utils/periods/compute_similarity_periods.py
@@ -10,9 +10,13 @@ def compute_similarity_periods(first_period, first_period_type, second_period, s
         first_integers, second_integers = extract_integers(first_period), extract_integers(second_period)
         if first_period_type in ["pdn", "pwn", "pmn", "pyn"]:
             distance = abs(first_integers[1] - second_integers[1])
+            if distance == 0.0:
+                return 1.0
             return 0.5 / distance**2
         if len(first_integers) == 1:
             distance = abs(first_integers[0] - second_integers[0])
+            if distance == 0.0:
+                return 1.0
             return 0.5 / distance**2
         else:
             distance = compute_interval_distance(first_integers, second_integers)
